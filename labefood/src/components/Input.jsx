2e17ca090205c692adf06feb207602607ea58b82
Jennifer Perucci de Almeida <jenniferperucci@hotmail.com>
2022-09-26 20:27:49 -0300

    limpeza

--- labefood/src/components/Input.jsx
+++ labefood/src/components/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, TextField } from '@mui/material'
 
-const Input = ({ label, placeholder, id, value, onChange, required }) => {
+const Input = ({ label, placeholder, id, value, onChange, required, pattern}) => {
     return (
         <Box sx={{ width: '100%' }}>
             <TextField sx={{ width: '100%', my: 1 }}
@@ -12,6 +12,7 @@ const Input = ({ label, placeholder, id, value, onChange, required }) => {
                 id={id}
                 label={label}
                 placeholder={placeholder}
+                pattern={pattern}
             // helperText="Some important text"      //quando der erro ativa
             />
         </Box>
--- labefood/src/pages/login/Login.jsx
+++ labefood/src/pages/login/Login.jsx
@@ -1,96 +1,93 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { ContainerForms } from '../../components/form/stylesForms'
-import { BASE_URL } from './../../constants/BASE_URL'
-import logo from './../../img/logo-rappi.png'
-import Input from '../../components/form/input'
-import { useForm } from '../../hooks/useForm'
-import useProtectedPage from '../../hooks/useProtectedPage'
-import { ControlImg, Container, Title, ControlButton, SpanControl } from './styledLogin'
+import { Container, Box } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import ButtonCustom from '../../components/ButtonCustom'
+import InputSenha from '../../components/InputSenha'
+import Titulo from '../../components/Titulo'
+import LogoImg from '../../components/Logo'
+import Input from '../../components/Input'
+import axios from 'axios'
+import { BASE_URL } from '../../constants/url'
 
+const Login = () => {
+  const navigate = useNavigate()
 
+  const goToSignUp = (navigate) => { navigate('/signup') }
+  const goToFeed = (navigate) => { navigate('/feed') }
 
+  const [values, setValues] = useState({
+    password: '',
+    email: '',
+    showPassword: false,
+  });
 
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
-const Login = ({ type, text, name, placeholder, onChange, required, value, }) => {
-  useProtectedPage();
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
 
-  const navigate = useNavigate();
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
-  const goToFeed = () => {
-    navigate('/feed')
-  }
-
-  const goToSignup = () => {
-    navigate('/signup')
-  }
-
-  const [ body, handleOnChange, clear] = useForm({ email: "", password: "" })
-
-  const loginOk = (e) => {
-    e.preventDefault()
-    console.log(body)
-    axios.post(`${BASE_URL}/login`, body,{
-      headers: {
-
-        'Content-Type': 'application/json'
-      }
-
-    }).then((response) => {
-      localStorage.setItem("token", response.data.token)
-      goToFeed();
-      clear();
-    }).catch((error) => {
-      console.log("Deu ruim!", error.response.data.message)
-      alert(error.response.data.message);
+  
+  const Log = () => {
+    axios.post(`${BASE_URL}/login`, {
+      "email": values.email,
+      "password": values.password
     })
-
+      .then((res) => {
+        localStorage.setItem('token', res.data.token)
+        if(!res.data.user.hasAddress){
+          navigate('/cadastroEndereco')
+        } else {
+          goToFeed(navigate)
+        }
+      }).catch((error) => {
+        console.log(error.response.data.message)
+      })
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    Log()
+  }
+
+  useEffect(() => {
+    Log()
+  }, [])
 
   return (
-    <Container>
-      <ContainerForms>
-        <ControlImg >
-          <img src={logo} alt="Logo-Rappi" />
-        </ControlImg >
-        <Title>
-          <h2>Entrar</h2>
-        </Title>
-        <form onSubmit={loginOk}>
-          <Input
-            type='email'
-            id='email'
-            name='email'
-            value={body.email}
-            onChange={handleOnChange}
-            placeholder={"email@email.com"}
-            required={required}
-          />
-          <Input
-            type='password'
-            value={body.password}
-            name='password'
-            id='password'
-            onChange={handleOnChange}
-            placeholder={"Mínimo 6 caracteres"}
-            required={required}
-
-          />
-          <ControlButton>
-            <button onClick={loginOk}>Entrar</button>
-          </ControlButton>
-        </form>
-        <SpanControl>
-          <span onClick={goToSignup}>Não possui cadastro? Clique aqui.</span>
-        </SpanControl>
-
-
-
-
-
-
-      </ContainerForms>
+    <Container maxWidth='lg' sx={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', pt: 8 }}>
+      <LogoImg />
+      <Titulo texto="Entrar" />
+      <Box onSubmit={handleSubmit} component='form' noValidate autoComplete="off" sx={{ width: '100%' }} >
+        <Input
+          value={values.email}
+          label='E-mail'
+          placeholder='email@email.com'
+          onChange={handleChange('email')}
+        />
+        <InputSenha
+          label='Senha'
+          placeholder='Mínimo 6 caracteres'
+          onChange={handleChange('password')}
+          onClick={handleClickShowPassword}
+          onMouseDown={handleMouseDownPassword}
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          testeee={values.showPassword ? <VisibilityOff /> : <Visibility />}
+        />
+        <ButtonCustom type='submit' value={'Entrar'} texto='Entrar' />
+      </Box>
+      <Titulo onClick={() => goToSignUp(navigate)} link={"Não possui cadastro? Clique aqui"} />
     </Container>
   )
 }
